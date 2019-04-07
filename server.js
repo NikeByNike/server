8c19a015973eb69c272213b9cc86c6cf6097c526
NikeByNike <workwithme@rambler.ru>
2019-04-07 23:05:35 +0300

    main

--- server.js
+++ server.js
@@ -1,11 +1,74 @@
-const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjId = require('mongodb').ObjectID;
+var express = require('express');
 
-const app = express();
+var app = express();
+
+let db;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello API");
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get("/products", (req, res) => {
+  db.collection("products").find().toArray((err, data) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.send(data);
+  });
+});
+
+app.get("/products/:id", (req, res) => {
+  db.collection("products").findOne({_id: ObjId(req.params.id)}, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.send(data);
+  });
+});
+
+app.post("/products", (req, res) => {
+  const product = {
+    title: req.body.title,
+  };
+  db.collection("products").insertOne(product, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.send(product);
+  });
+});
+
+app.delete("/products/:id", (req, res) => {
+  db.collection("products").deleteOne({_id: ObjId(req.params.id)}, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    res.sendStatus(200);
+  });
+});
+
+const url = 'mongodb://localhost:27017';
+
+const dbName = 'myAPI';
+
+const client = new MongoClient(url, { useNewUrlParser: true } );
+
+client.connect((err, client) => {
+  if (err) {
+    console.log(err);
+  }
+  db = client.db(dbName);
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
 });
